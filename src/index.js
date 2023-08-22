13c5aee1798407696ad67b423a4c4f49adaa6dce
Lartg <roc.gerrety@gmail.com>
2023-08-22 15:53:15 -0800

    most functions done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function capitalize(str) {
   return str[0].toUpperCase() + str.slice(1)
 }
 
-function allCaps(str){
+function allCaps(str) {
   return str.toUpperCase()
 }
 
@@ -12,30 +12,66 @@ function capitalizeWords(str) {
   for (word in operStr) {
     newStr += `${capitalize(operStr[word])} `
   }
+  newStr = newStr.slice(0,-1)
   return newStr
 }
 
-function capitalizeHeadline(str){
+function removeExtraSpaces(str) {
+  const chars = str.split('')
+  let newStr = ''
+  for (char in chars) {
+    if (char != length(chars)) {
+      if (chars[char] == ' ') {
+        if (chars[char + 1] == ' ') {
+          continue
+        }
+      }
+    }
+    newStr += chars[char]
+  }
+  return newStr
+}
+
+function kebabCase(str) {
+  const operStr = removeExtraSpaces(str)
+  const chars = operStr.split('')
+  let newStr = ''
+  for(char in chars){
+    if (chars[char] == ' '){
+      newStr += '-'
+      continue
+    }
+    newStr += chars[char]
+  }
+  return newStr
+}
+function snakeCase(str) {
+  const operStr = removeExtraSpaces(str)
+  const chars = operStr.split('')
+  let newStr = ''
+  for(char in chars){
+    if (chars[char] == ' '){
+      newStr += '_'
+      continue
+    }
+    newStr += chars[char]
+  }
+  return newStr
+}
+function camelCase(str) {
+  let operStr = str.split(" ")
+  let newStr = ""
+  for (word in operStr) {
+    newStr += `${capitalize(operStr[word])}`
+  }
+  return newStr
+}
+function shift(str, nChars) {
   return
 }
-function removeExtraSpaces(str){
+function makeHashTag(str) {
   return
 }
-function kebabCase(str){
-  return
-}
-function snakeCase(str){
-  return
-}
-function camelCase(str){
-  return
-}
-function shift(str){
-  return
-}
-function makeHashTag(str){
-  return
-}
-function isEmpty(str){
+function isEmpty(str) {
   return
 }
